1031c154b995667c9923e3c26bd75e80aa94c8ab
arnabkarsarkar-okta <73860848+arnabkarsarkar-okta@users.noreply.github.com>
2024-06-11 18:18:03 -0600

    Update overrides.php

--- workbench/config/overrides.php
+++ workbench/config/overrides.php
@@ -26,8 +26,8 @@ $config["oauthConfigs"]["default"] = array(
                                            ),
                                            "test.salesforce.com" => array(
                                                "label" => "Sandbox",
-                                               "key" => "3MVG9x2D7w6gNN5lB438vZDp5jQMLY7Imji6FPN31MpU5DJG5c2gSVAygFjFg7pTioLObFgprEoAyad5ndvhU",
-                                               "secret" => "A2745FAAC43141A44B8671359D1BE1F01087D67EF92A770CE50489ACE7C3FB86"
+                                               "key" => "",
+                                               "secret" => ""
                                            )
                                        );
 
